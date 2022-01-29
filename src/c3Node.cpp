420b12e8dd30e3e05605924ed9876fd4e22d111a
Andrew Sweeney <andrewsweeney@system76-pc.localdomain>
2022-01-28 18:30:55 -0600

    Started work on MQTT client stuff

---
+++ src/c3Node.cpp
@@ -0,0 +1,12 @@
+#include "c3Node.h"
+
+void MicroCloudNode::C3Node::c3Setup()
+{
+    //mesh.setRoot(true);
+}
+
+void MicroCloudNode::C3Node::c3Loop()
+{
+    c3Setup();
+
+}
--- src/c3Node.h
+++ src/c3Node.h
@@ -2,5 +2,25 @@
 #define C3NODE_H
 
 #include "node.h"
+#include "Arduino.h"
+#include "painlessMesh.h"
+#include <PubSubClient.h>
+#include <WiFiClient.h>
 
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+IPAddress getLocalIP();
+IPAddress myIP(0,0,0,0);
+IPAddress mqttBroker(192,168,1,1);
+WiFiClient wifiClient;
+PubSubClient mqttClient(mqttBroker, 1883, mqttCallback, wifiClient);
+
+namespace MicroCloudNode
+{
+    namespace C3Node
+    {
+        void c3Setup();
+        void c3Loop();
+        Task c3Task(TASK_SECOND * 1 , TASK_FOREVER, &c3Loop);
+    }
+}
 #endif // C3NODE_H
