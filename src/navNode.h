215292a57bcdea1f47d475183ee4f1e7944777d7
aapollard12 <aap0029@uah.edu>
2022-02-03 21:12:01 -0600

    also added possible navNode.h header information

--- src/navNode.h
+++ src/navNode.h
@@ -1,6 +1,49 @@
+/*
+Author: Austin Pollard
+CPE496/496
+File: navNode.h
+Description: 
+     
+*/
 #ifndef NAVNODE_H
 #define NAVNODE_H
 
 #include "node.h"
+
+#define SIZE  20 // number cols/rows in Grid
+
+// Function Declaration------------------------
+void readMap(char* file , int [SIZE][SIZE]);//DONE
+void printMap(int [SIZE][SIZE]);//DONE
+
+int get_x_pos(int x_pos);       //can get x pos from text file
+int get_y_pos(int y_pos);       // can get pos from text file
+                                    // call  get_x_pos(caravan.x_pos_car); maybe
+bool get_direction(bool direction);
+int horiz_dist(int x_start, int x_dest);
+int vert_distance(int y_start, int y_dest);
+void move_direction(bool direct);   // only moves one direction at a time.
+void map_redraw(int map[SIZE][SIZE]); // redraws matrix based off direction value
+
+struct caravan  // could be car if we want multiple seperate vehicles
+{
+    int x_pos_car;
+    int y_pos_car;
+    int fuel;   //could have certain amount of fuel tofind most effecient way.
+    bool on_path
+    struct direction        // for direction we could just
+    {                         // horizontal and veritcal and move diaganol
+        bool north;            // if map arrays updated horizontally it might
+        bool south;             // a little more difficult
+        bool west;
+        bool east;
+    }
+}
+
+struct destination
+{
+    int x_pos_dest;
+    int x_pos_dest;
+}
 
 #endif // NAVNODE_H
